bcff7a3554ca43fcd22e5e01d8e2f1fbf4220f75
AngelArevalo <aarevalo@dvconsultores.com>
2023-05-11 17:02:32 -0400

    Initial commit.

---
+++ src/DinoirProyectICP_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { DinoirProyectICP_backend } from "../../declarations/DinoirProyectICP_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await DinoirProyectICP_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
